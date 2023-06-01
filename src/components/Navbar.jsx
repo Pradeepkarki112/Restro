1307086e89efa6942c4f1560a2ddc4cac06a4bfb
Pradeepkarki112 <pradeepkarki112@gmail.com>
2023-06-01 15:27:56 +0545

    navbar started

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,29 @@
-import React from "react";
-import { AiOutlineClose, AiOutlineMenu, AiOutlineSearch, AiOutlineShoppingCart } from "react-icons/ai";
+import React, { useState } from "react";
+import {
+  AiOutlineClose,
+  AiOutlineMenu,
+  AiOutlineSearch,
+  AiOutlineShoppingCart,
+  AiFillTag,
+} from "react-icons/ai";
+import { BsFillSaveFill } from "react-icons/bs";
+import { TbTruckDelivery } from "react-icons/tb";
+import { FaUserFriends, FaWallet } from "react-icons/fa";
+import { MdFavorite, MdHelp } from "react-icons/md";
+import { motion } from "framer-motion";
 
 function Navbar() {
+  const [nav, setNav] = useState(false);
+
   return (
-    <div className="flex justify-between items-center max-w-[1640px] mx-auto p-4">
+    <motion.div
+      initial={{ opacity: 0, x: 200 }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, x: 200 }}
+      className="flex justify-between items-center max-w-[1640px] mx-auto p-4"
+    >
       {/* left side */}
-      <div className="flex gap-5 items-center">
+      <div onClick={() => setNav(!nav)} className="flex gap-5 items-center">
         <AiOutlineMenu className="text-3xl" />
         <h1 className="text-2xl sm:text-3xl lg:4xl">
           Best<span className="font-bold pl-1">Eats</span>
@@ -19,29 +37,70 @@ function Navbar() {
       {/* Search Input */}
       <div className=" border rounded-full p-2 flex gap-2 items-center w-[200px] sm:w-[400px] lg:w-[500px]">
         <AiOutlineSearch className="text-2xl" />
-        <input type="text" placeholder="Search here..." className="focus:outline-none w-full" />
+        <input
+          type="text"
+          placeholder="Search here..."
+          className="focus:outline-none w-full"
+        />
       </div>
 
       {/* Cart */}
       <div className=" p-1 px-3 rounded-2xl bg-black text-white flex items-center gap-1">
-        <div><AiOutlineShoppingCart /></div>
+        <div>
+          <AiOutlineShoppingCart />
+        </div>
         <p>Cart</p>
       </div>
 
       {/* Overlay */}
-      <div className="w-full fixed top-0 left-0 bg-black/80 z-10 h-screen">
-      </div>
+      {nav ? (
+        <div className="w-full fixed top-0 left-0 bg-black/80 z-10 h-screen"></div>
+      ) : (
+        ""
+      )}
 
       {/* Side drawer menu */}
-      <div className="z-10 fixed bg-white text-black h-screen top-0 left-0 w-[300px]">
-        <AiOutlineClose className="text-4xl absolute top-4 right-4 cursor-pointer" />
+      <div
+        className={
+          nav
+            ? "z-10 fixed bg-white h-screen top-0 left-0 w-[300px]"
+            : "z-10 fixed bg-white h-screen top-0 left-[-100%] w-[300px]"
+        }
+      >
+        <AiOutlineClose
+          onClick={() => setNav(!nav)}
+          className="text-4xl absolute top-4 right-4 cursor-pointer"
+        />
         <h2 className="p-4 text-2xl">
           Best<span className="pl-1 font-bold">Eats</span>
         </h2>
-
+        <div>
+          <ul className=" p-4 text-gray-800">
+            <li className="text-xl py-4 flex">
+              <TbTruckDelivery className="text-2xl mr-4" /> Orders
+            </li>
+            <li className="text-xl py-4 flex">
+              <MdFavorite className="text-2xl mr-4" /> Favorites
+            </li>
+            <li className="text-xl py-4 flex">
+              <FaWallet className="text-2xl mr-4" /> Wallet
+            </li>
+            <li className="text-xl py-4 flex">
+              <MdHelp className="text-2xl mr-4" /> Help
+            </li>
+            <li className="text-xl py-4 flex">
+              <AiFillTag className="text-2xl mr-4" /> Promotions
+            </li>
+            <li className="text-xl py-4 flex">
+              <BsFillSaveFill className="text-2xl mr-4" /> Best Ones
+            </li>
+            <li className="text-xl py-4 flex">
+              <FaUserFriends className="text-2xl mr-4" /> Invite Friends
+            </li>
+          </ul>
+        </div>
       </div>
-
-    </div>
+    </motion.div>
   );
 }
 
