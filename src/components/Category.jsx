82251e423080284eefee5396846ffa4177c2b848
Pradeepkarki112 <pradeepkarki112@gmail.com>
2023-06-02 13:01:09 +0545

    added

---
+++ src/components/Category.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Category = () => {
+  return (
+    <div className="max-w-[1640px] mx-auto">
+      <div className="font-bold text-3xl text-orange-500 flex flex-col items-center">
+        <div>Top Rated Menu Items</div>
+      </div>
+    </div>
+  );
+};
+
+export default Category;
--- src/components/HeadlineCards.jsx
+++ src/components/HeadlineCards.jsx
@@ -5,43 +5,42 @@ import photo3 from "../assets/photo3.jpg";
 
 const HeadlineCards = () => {
   return (
-    <div className="max-w-screen-xl mx-auto grid grid-cols-2 sm:grid-cols-3 gap-3 px-4 py-8 text-white text-xs font-bold">
-  {/* Card */}
-  <div className="relative">
-    <div className="absolute bg-black/40 w-full h-full"></div>
-    <div className="absolute pt-[10%] pl-[2%] absolute-content">
-      <div>
-        Sun's Out, BOGO's Out
-        <br />
-        <span className="font-medium">Through 8/26</span>
+    <div className="max-w-screen-2xl mx-auto grid grid-cols-2 sm:grid-cols-3 gap-3 px-4 py-8 text-white text-xs font-bold sm:text-sm md:text-md lg:text-lg">
+      {/* Card */}
+      <div className="relative">
+        <div className="absolute z-2 bg-black/40 w-full h-full"></div>
+        <div className="absolute z-3 ">
+          <div>
+            Sun's Out, BOGO's Out
+            <br />
+            <span className="font-medium">Through 8/26</span>
+          </div>
+          <div>Order Now</div>
+        </div>
+        <img src={photo1} alt="photo1" />
+        <div className="absolute"></div>
       </div>
-      <button>Order Now</button>
+
+      <div className="relative">
+        <div className="absolute bg-black/40 w-full h-full"></div>
+        <p className="absolute pt-[10%] pl-[2%]">
+          New Restaurants
+          <br />
+          <span className="font-medium">Added Daily</span>
+        </p>
+        <img src={photo2} alt="photo2" />
+      </div>
+
+      <div className="relative">
+        <div className="absolute bg-black/40 w-full h-full"></div>
+        <p className="absolute pt-[10%] pl-[2%]">
+          We Deliver Desserts Too
+          <br />
+          <span className="font-medium">Tasty Treats</span>
+        </p>
+        <img src={photo3} alt="photo3" />
+      </div>
     </div>
-    <img src={photo1} alt="photo1" />
-    <div className="absolute"></div>
-  </div>
-
-  <div className="relative">
-    <div className="absolute bg-black/40 w-full h-full"></div>
-    <p className="absolute pt-3 pl-1 absolute-content">
-      New Restaurants
-      <br />
-      <span className="font-medium">Added Daily</span>
-    </p>
-    <img src={photo2} alt="photo2" />
-  </div>
-
-  <div className="relative">
-    <div className="absolute bg-black/40 w-full h-full"></div>
-    <p className="absolute pt-3 pl-1 absolute-content">
-      We Deliver Desserts Too
-      <br />
-      <span className="font-medium">Tasty Treats</span>
-    </p>
-    <img src={photo3} alt="photo3" />
-  </div>
-</div>
-
   );
 };
 
